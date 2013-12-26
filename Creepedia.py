eda2688e59fbca47b243e34483e5191de3ab74fc
Oz Tamir <TheOzTamir@gmail.com>
2013-12-26 19:42:45 +0200

    Added coolness

--- Creepedia.py
+++ Creepedia.py
@@ -2,6 +2,7 @@ from PIL import Image
 from PIL.ExifTags import TAGS, GPSTAGS
 import requests
 from flask import Flask
+from WikiParse import WikiParse
 
 app = Flask(__name__)
 
@@ -64,17 +65,48 @@ def articles(lat, lon, radius, limit, type_):
 exif = get_exif_data('IMG2.JPG')
 lat, lon = gps_coord(exif)
 
-articles = articles(lat, lon, 1000, 10, '')[0]
-print articles
+articles = articles(lat, lon, 1000, 1, '')
+infoBox = []
+'''
+for article in articles:
+	if 'url' in article:
+		parse = WikiParse(article['url'])
+		title = parse.parseTitle()
+		image = parse.parsePicture()
+'''
+
+def getTableItem(articles):
+	table = '<table><tbody><tr>'
+	for article in articles:
+		if 'url' in article:
+			parse = WikiParse(article['url'])
+			picture = parse.parsePicture()
+			table += '<td><img src="' + picture + '">'
+			title = parse.parseTitle()
+			table += '<a href="' + article['url'] + '"><h3>' + str(title) + '</h3></a></td>'
+	table += '</tr><br></tbody></table><br><br>'
+	return str(table)
 
 @app.route('/')
 def stock():
-	return ''' <center><h1 style="font-family:sans-serif;"> Creepedia </h1> <div>
-<div>  <img src="http://maps.googleapis.com/maps/api/streetview?size=300x300&location=%s&heading=235&sensor=false" /> 
-		<img src="http://maps.googleapis.com/maps/api/staticmap?center=%s&zoom=17&size=600x300&sensor=false" /> </div>
-<iframe src="%s" width="900px" height="300px" /></div> </center>
-		
-		''' % (str(lat)+','+str(lon),str(lat)+','+str(lon), articles['url'] )
+	return '''
+	<center>
+	<h1 style="font-family:sans-serif;"> Creepedia </h1>
+	<hr>
+	<div>
+		<div>
+		%s
+		</div>
+		<hr>
+		<br><br>
+		<div> 
+			<img src="http://maps.googleapis.com/maps/api/streetview?size=300x300&location=%s&heading=235&sensor=false" /> 
+			<img src="http://maps.googleapis.com/maps/api/staticmap?center=%s&zoom=17&size=600x300&sensor=false" />
+		</div>
+		<iframe src="%s" width="900px" height="300px" />
+	</div>
+	</center>
+		''' % (str(getTableItem(articles)), str(lat)+','+str(lon),str(lat)+','+str(lon), articles[0]['url'] )
 
 if __name__=='__main__':
 	app.run()
