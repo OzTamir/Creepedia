e721b5efc45bcf1f4edcaf6059fbe6a0a9be6b79
OzTamir <TheOzTamir@gmail.com>
2013-12-28 19:47:09 +0200

    Complete Flask web-app relese

--- routes.py
+++ routes.py
@@ -1,24 +1,43 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, redirect, url_for
 from WikiParse import WikiParse, articleParse
-from Creepedia import *
+import Creepedia
+from werkzeug import secure_filename
+import os
+
+UPLOAD_FOLDER = 'static/uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif', 'JPG', 'PNG'])
 
 app = Flask(__name__)
-app.secret_key = 'hackath0n'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-exif = get_exif_data('IMG2.JPG')
-lat, lon = gps_coord(exif)
-coord = str(lat) + ',' + str(lon)
-parsedWiki = articles(lat, lon, 1000, 2, '')
-articles = []
-for i in range(len(parsedWiki)):
-    if 'url' in parsedWiki[i]:
-        articles.append(articleParse(parsedWiki[i]['url']))
 
-@app.route('/')
+@app.route('/creep')
 def home():
-  return render_template('home.html' , coord = coord, url = articles[0].url, articles = articles)
+    global filename
+    with app.open_resource('static/uploads/' + filename) as img:
+        exif = Creepedia.get_exif_data(img)
+        lat, lon = Creepedia.gps_coord(exif)
+        coord = str(lat) + ',' + str(lon)
+        parsedWiki = Creepedia.articles(lat, lon, 1000, 2, '')
+        articles = []
+        for i in range(len(parsedWiki)):
+            if 'url' in parsedWiki[i]:
+                articles.append(articleParse(parsedWiki[i]['url']))
+        return render_template('home.html' , coord = coord, url = articles[0].url, articles = articles)
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            global filename
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('home'))
+    return render_template('upload.html')
 if __name__ == '__main__':
     app.run(debug = True)
-
-# str(lat) + ',' + str(lon), str(lat) + ',' + str(lon), articles[0]['url']
