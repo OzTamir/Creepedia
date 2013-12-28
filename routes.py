e836ddee75ca601496481716e470aca64582d581
OzTamir <OzTamir@users.noreply.github.com>
2013-12-28 21:32:02 +0200

    Changed app.run()

--- routes.py
+++ routes.py
@@ -45,4 +45,5 @@ def upload_file():
             return redirect(url_for('home'))
     return render_template('upload.html')
 if __name__ == '__main__':
-    app.run(debug = True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
